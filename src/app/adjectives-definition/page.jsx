f55827e7a7e07106d2cee840b3e4f0952d1aad3c
Chris <chrisdietrich3666@gmail.com>
2023-11-25 20:29:00 -0500

    styled adjective def page

--- src/app/adjectives-definition/page.jsx
+++ src/app/adjectives-definition/page.jsx
@@ -16,7 +16,7 @@ import styles from "../styles/content-page.module.css";
 export default function AdjectiveDefinition() {
   return (
     <Container>
-       <Stack className={styles.page_title}>
+      <Stack className={styles.page_title}>
         <Typography
           component="h1"
           variant="h1"
@@ -27,49 +27,57 @@ export default function AdjectiveDefinition() {
       </Stack>
       <Stack className={styles.page_body}>
         <Typography className={styles.grammar_explanation_paragraph}>
-        Adjectives are used to describe or modify nouns or pronouns, providing
+          Adjectives are used to describe or modify nouns or pronouns, providing
           more information about their qualities, characteristics, or
           attributes. In these examples, the adjectives are in bold.
         </Typography>
       </Stack>
-  
-      <div className="container">
-        <div className="row">
-          <div className={`col-md-4 ${styles.definition_picture}`}>
-            <div className={styles.picture_wrapper}>
-              <Image
-                height={250}
-                width={250}
-                src="/images/happy.jpg"
-                alt="happy child"
-              />
-            </div>
-            <p className={styles.picture_subtitle}>Happy</p>
-          </div>
-          <div className={`col-md-4 ${styles.definition_picture}`}>
-            <div className={styles.picture_wrapper}>
-              <Image
-                height={250}
-                width={250}
-                src="/images/small.jpg"
-                alt="small dog"
-              />
-            </div>
-            <p className={styles.picture_subtitle}>Small</p>
-          </div>
-          <div className={`col-md-4 ${styles.definition_picture}`}>
-            <div className={styles.picture_wrapper}>
-              <Image
-                height={250}
-                width={250}
-                src="/images/strong.jpg"
-                alt="strong person"
-              />
-            </div>
-            <p className={styles.picture_subtitle}>Strong</p>
-          </div>
-        </div>
-      </div>
+      <Stack direction="row" spacing={6}>
+        <Box>
+          <Box>
+            <Image
+              style={{
+                objectFit: "contain",
+                borderRadius: "20%",
+                top: "50%",
+                left: "50%",
+              }}
+              height={250}
+              width={250}
+              src="/images/happy.jpg"
+              alt="happy child"
+            />
+          </Box>
+
+          <Typography variant="h5" mt={2} sx={{ textAlign: "center" }}>
+            Happy
+          </Typography>
+        </Box>
+
+        <Box>
+          <Image
+            height={250}
+            width={250}
+            src="/images/small.jpg"
+            alt="small dog"
+          />
+          <Typography variant="h5" mt={2}>
+            Small
+          </Typography>
+        </Box>
+
+        <Box>
+          <Image
+            height={250}
+            width={250}
+            src="/images/strong.jpg"
+            alt="strong person"
+          />
+          <Typography variant="h5" mt={2}>
+            Strong
+          </Typography>
+        </Box>
+      </Stack>
     </Container>
   );
 }
