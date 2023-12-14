8c94accecedc5c1a3460999c7c1008122a630101
Chris <chrisdietrich3666@gmail.com>
2023-12-13 20:18:11 -0500

    updated layout for expressions

--- src/app/other-topics/idiomatic-expressions/layout.jsx
+++ src/app/other-topics/idiomatic-expressions/layout.jsx
@@ -1,61 +1,94 @@
 import React from "react";
 import Link from "next/link";
 
+import {
+  Container,
+  Grid,
+  Stack,
+  ListItem,
+  ListItemText,
+  List,
+  Typography,
+} from "@mui/material";
+
 import styles from "../../styles/layout.module.css";
 
 const Layout = ({ children }) => {
   return (
-    <div>
-      <section className={styles.main_container}>
-        <nav className={styles.sidebar}>
-          <ul className={styles.list_container}>
-            <li>
-              <Link
-                className={`${styles.link}`}
-                href="/other-topics/idiomatic-expressions"
-              >
-                Idiomatic Expressions
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={styles.link}
-                href="/other-topics/idiomatic-expressions/expressions-list-1"
-              >
-                Expressions List 1
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={styles.link}
-                //
-                href="/other-topics/idiomatic-expressions/expressions-list-2"
-              >
-                Expressions List 2
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={styles.link}
-                //
-                href="/other-topics/idiomatic-expressions/expressions-list-3"
-              >
-                Expressions List 3
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={styles.link}
-                //
-                href="/other-topics/idiomatic-expressions/expressions-list-4"
-              >
-                Expressions List 4
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <div className={styles.main_content}>{children}</div>
-      </section>
+    <div style={{ backgroundColor: "#f3f0f0" }}>
+      <Container>
+        <Grid container spacing={2}>
+          <Grid item xs={3} >
+            <Stack
+              mt={3.75}
+              sx={{
+                height: "100vh", 
+                borderRadius: "9px",
+                backgroundColor: "#fff",
+              }}
+            >
+              <List>
+                <ListItem sx={{ display: "list-item" }}>
+                  <ListItemText>
+                    <Link
+                      className={`${styles.link}`}
+                      href="/other-topics/idiomatic-expressions"
+                    >
+                      Idiomatic Expressions
+                    </Link>
+                  </ListItemText>
+                </ListItem>
+                <ListItem sx={{ display: "list-item" }}>
+                  <ListItemText>
+                    <Link
+                      className={styles.link}
+                      href="/other-topics/idiomatic-expressions/expressions-list-1"
+                    >
+                      Expressions List 1
+                    </Link>
+                  </ListItemText>
+                </ListItem>
+                <ListItem sx={{ display: "list-item" }}>
+                  <ListItemText>
+                    <Link
+                      className={styles.link}
+                      //
+                      href="/other-topics/idiomatic-expressions/expressions-list-2"
+                    >
+                      Expressions List 2
+                    </Link>
+                  </ListItemText>
+                </ListItem>
+                <ListItem sx={{ display: "list-item" }}>
+                  <ListItemText>
+                    <Link
+                      className={styles.link}
+                      //
+                      href="/other-topics/idiomatic-expressions/expressions-list-3"
+                    >
+                      Expressions List 3
+                    </Link>
+                  </ListItemText>
+                </ListItem>
+                <ListItem sx={{ display: "list-item" }}>
+                  <ListItemText>
+                    <Link
+                      className={styles.link}
+                      //
+                      href="/other-topics/idiomatic-expressions/expressions-list-4"
+                    >
+                      Expressions List 4
+                    </Link>
+                  </ListItemText>
+                </ListItem>
+              </List>
+            </Stack>
+          </Grid>
+          <Grid item xs={9}>
+            <div className={styles.main_content}>{children}</div>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 };
